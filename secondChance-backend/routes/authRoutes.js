6856e58b225fbeca9f99cd0f47455d1e26986412
curserastudent <mbustama@uoc.edu>
2025-12-17 14:52:56 -0500

    Develop Back-end Integration for Login

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -5,6 +5,7 @@ const connectToDatabase = require('../models/db');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const logger = require('../logger');
+const { body, validationResult } = require('express-validator');
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -53,5 +54,51 @@ router.post('/register', async (req, res) => {
          return res.status(500).send('Internal server error');
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ error: 'Email and password are required' });
+        }
+
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+
+        const theUser = await collection.findOne({ email });
+        if (!theUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const result = await bcryptjs.compare(password, theUser.password);
+        if (!result) {
+            logger.error('Passwords do not match');
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        if (!JWT_SECRET) {
+            throw new Error('JWT_SECRET is not defined');
+        }
+
+        const payload = {
+            user: {
+                id: theUser._id.toString(),
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+        res.json({
+            authtoken,
+            userName: theUser.firstName,
+            userEmail: theUser.email,
+        });
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+
+    }
+});
+
+
 
 module.exports = router;
